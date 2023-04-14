ef828cbe43141d468a048dfc1815b1afb61e0106
Tom Scholtz <tom.scholtz@lifeq.com>
2023-04-14 18:11:10 +0200

    added a consumer

---
+++ src/go/bench/bench-consumer/app/cmd/consumer.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/tomschdev/kafka-bench/src/proto/person"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	// Create a new Kafka reader
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "people",
+	})
+
+	// Handle OS signals to gracefully shutdown the reader
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(signals)
+	defer close(signals)
+
+	// Start consuming messages
+	for {
+		// Read the next message from the Kafka topic
+		msg, err := r.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatalf("error reading message: %v", err)
+		}
+
+		// Unmarshal the message into a Person proto
+		var person person.Person
+		err = person.Unmarshal(msg.Value)
+		if err != nil {
+			log.Fatalf("error unmarshalling message: %v", err)
+		}
+
+		// Do something with the person data
+		fmt.Printf("Received person: %v\n", person)
+
+		// Check for OS signals to shutdown the reader
+		select {
+		case sig := <-signals:
+			log.Printf("Received %v signal, shutting down...\n", sig)
+			err := r.Close()
+			if err != nil {
+				log.Fatalf("error closing Kafka reader: %v", err)
+			}
+			return
+		default:
+		}
+	}
+}
---
+++ src/go/bench/bench-consumer/go.mod
@@ -0,0 +1,14 @@
+module github.com/tomschdev/kafka-bench/src/go/bench/bench-consumer
+
+go 1.19
+
+require (
+	github.com/segmentio/kafka-go v0.4.39
+	github.com/tomschdev/kafka-bench v0.0.0-20230414154918-39a7d40a0c86
+)
+
+require (
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
